ae5f1820d79eed81886f36fb88524a0b39401063
zoujun <13683366156@163.com>
2024-11-18 17:14:52 +0800

    11

--- bio-drqi-manage/bio-drqi-manage-main/src/main/java/com/bio/drqi/manage/BioDrqiManageMain.java
+++ bio-drqi-manage/bio-drqi-manage-main/src/main/java/com/bio/drqi/manage/BioDrqiManageMain.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"com.bio.drqi","com.bio.common","com.bio.print"})
+@SpringBootApplication(scanBasePackages = {"com.bio.drqi","com.bio.common","com.bio.print","com.bio.base"})
 @EnableDiscoveryClient
 @MapperScan(basePackages = "com.bio.drqi.mapper")
 @EnableFeignClients(basePackages = {"com.bio.drqi","com.bio.print.api","com.bio.base"})

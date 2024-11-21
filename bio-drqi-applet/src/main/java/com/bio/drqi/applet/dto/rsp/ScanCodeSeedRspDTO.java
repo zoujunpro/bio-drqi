c1f1ce9c97afa3e34671eb7fede5651fdbca10d5
zoujun <13683366156@163.com>
2024-11-21 09:37:33 +0800

    11

---
+++ bio-drqi-applet/src/main/java/com/bio/drqi/applet/dto/rsp/ScanCodeSeedRspDTO.java
@@ -0,0 +1,186 @@
+package com.bio.drqi.applet.dto.rsp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class ScanCodeSeedRspDTO {
+
+    private String projectCode;
+
+    private String projectName;
+
+    private String subProjectCode;
+
+    private String subProjectName;
+
+    private String vectorTaskCode;
+
+    private String vectorTaskName;
+
+    private String transformName;
+
+    private String transformCode;
+
+    private List<Seed> seedList=new ArrayList<>();
+
+    @Data
+    public static class Seed {
+
+        /**
+         * 种植编号
+         */
+        private String plantNum;
+
+        /**
+         * 种子编号
+         */
+        private String seedNum;
+
+        /**
+         * 上一代种子编号
+         */
+        private String parentNum;
+
+        /**
+         * 父本信息
+         */
+        private String fartherInfo;
+
+        /**
+         * 母本信息
+         */
+        private String matherInfo;
+
+        /**
+         * 代次
+         */
+        private String generation;
+
+        /**
+         * 项目物种
+         */
+        private String speciesCode;
+
+        /**
+         * 项目物种
+         */
+        private String speciesName;
+
+        /**
+         * 品种
+         */
+        private String breedCode;
+
+        /**
+         * 品种
+         */
+        private String breedName;
+
+        /**
+         * 授粉方式
+         */
+        private String pollinationMethod;
+
+        /**
+         * 种子类型  自交/杂交
+         */
+        private String seedType;
+
+        /**
+         * 收获方式，单珠和混珠
+         */
+        private String harvestType;
+
+        /**
+         * 收获时间
+         */
+        private String harvestTime;
+
+        /**
+         * 种子数量
+         */
+        private BigDecimal seedNumber;
+
+        /**
+         * 计量单位g/kg/粒
+         */
+        private String unit;
+
+        /**
+         * 种子来源（1 CER/ 2 温室/3 大田/4 外单位）
+         */
+        private String sourceType;
+
+
+        /**
+         * 生产地点（天津/海南/新乡）
+         */
+        private String productionLocationName;
+
+
+        /**
+         * 库位编号
+         */
+        private String stockLocationNum;
+
+        /**
+         * 提交人ID
+         */
+        private Integer submitUserId;
+
+        /**
+         * 提交人姓名
+         */
+        private String submitUserName;
+
+        /**
+         * 创建日期
+         */
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private Date createTime;
+
+        /**
+         * 更新日期
+         */
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private Date updateTime;
+
+        /**
+         * 备注
+         */
+        private String remarks;
+
+        /**
+         * 入库时库存
+         */
+        private BigDecimal totalNumber;
+
+        /**
+         * 基因型性状
+         */
+        private String geneticCharacter;
+
+        /**
+         * 别名
+         */
+        private String aliasName;
+
+        /**
+         * 基因类型
+         */
+        private String geneType;
+
+        /**
+         * 材料类型
+         */
+        private String materialType;
+
+    }
+
+}
--- bio-drqi-applet/src/main/java/com/bio/drqi/applet/service/codescan/template/SeedCodeScanService.java
+++ bio-drqi-applet/src/main/java/com/bio/drqi/applet/service/codescan/template/SeedCodeScanService.java
@@ -2,20 +2,24 @@ package com.bio.drqi.applet.service.codescan.template;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.bio.common.core.dto.BusinessException;
+import com.bio.drqi.applet.dto.rsp.ScanCodeSeedRspDTO;
 import com.bio.drqi.applet.dto.rsp.ScanCodeTransformRspDTO;
 import com.bio.drqi.applet.service.codescan.AbstractBaseCodeScanService;
+import com.bio.drqi.applet.service.codescan.dto.SeedUniqueCodeDTO;
 import com.bio.drqi.applet.service.codescan.dto.TransformUniqueCodeDTO;
 import com.bio.drqi.domain.*;
 import com.bio.drqi.mapper.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 转化扫码
  */
 @Service
-public class SeedCodeScanService extends AbstractBaseCodeScanService<TransformUniqueCodeDTO, ScanCodeTransformRspDTO> {
+public class SeedCodeScanService extends AbstractBaseCodeScanService<SeedUniqueCodeDTO, ScanCodeSeedRspDTO> {
 
     @Resource
     private CerVectorTaskTbMapper cerVectorTaskTbMapper;
@@ -32,42 +36,72 @@ public class SeedCodeScanService extends AbstractBaseCodeScanService<TransformUn
     @Resource
     private CerVectorGroupTbMapper cerVectorGroupTbMapper;
 
+    @Resource
+    private SeedStockTbMapper seedStockTbMapper;
+
+
     @Override
-    public TransformUniqueCodeDTO parseUniqueCode(String uniqueCode) {
+    public SeedUniqueCodeDTO parseUniqueCode(String uniqueCode) {
         String[] uniqueCodeArr = uniqueCode.split("\\|");
-        TransformUniqueCodeDTO transformUniqueCodeDTO = new TransformUniqueCodeDTO();
-        transformUniqueCodeDTO.setTransformCode(uniqueCodeArr[1]);
-        transformUniqueCodeDTO.setVectorTaskCode(uniqueCodeArr[0]);
-        return transformUniqueCodeDTO;
+        SeedUniqueCodeDTO seedUniqueCodeDTO = new SeedUniqueCodeDTO();
+        seedUniqueCodeDTO.setSeedNum(uniqueCodeArr[1]);
+        return seedUniqueCodeDTO;
     }
 
 
     @Override
-    public ScanCodeTransformRspDTO dealCodeContent(TransformUniqueCodeDTO transformUniqueCodeDTO) {
-        CerVectorTaskTb cerVectorTaskTb = cerVectorTaskTbMapper.selectOneByVectorTaskCode(transformUniqueCodeDTO.getVectorTaskCode());
-        if (cerVectorTaskTb == null) {
-            throw new BusinessException("实施方案查询不到:" + transformUniqueCodeDTO.getVectorTaskCode());
+    public ScanCodeSeedRspDTO dealCodeContent(SeedUniqueCodeDTO seedUniqueCodeDTO) {
+        ScanCodeSeedRspDTO scanCodeSeedRspDTO = new ScanCodeSeedRspDTO();
+        List<SeedStockTb> seedStockTbList = findSeed(seedUniqueCodeDTO.getSeedNum(), new ArrayList<SeedStockTb>());
+        for (SeedStockTb seedStockTb:seedStockTbList){
+            ScanCodeSeedRspDTO.Seed seed = new ScanCodeSeedRspDTO.Seed();
+            seed.setPlantNum(seedStockTb.getPlantNum());
+            seed.setSeedNum(seedStockTb.getSeedNum());
+            seed.setParentNum(seedStockTb.getParentNum());
+            seed.setFartherInfo(seedStockTb.getFartherInfo());
+            seed.setMatherInfo(seedStockTb.getMatherInfo());
+            seed.setGeneration(seedStockTb.getGeneration());
+            seed.setSpeciesCode(seedStockTb.getSpecies());
+            seed.setSpeciesName(null);
+            seed.setBreedCode(seedStockTb.getBreedCode());
+            seed.setBreedName(null);
+            seed.setPollinationMethod(seedStockTb.getPollinationMethod());
+            seed.setSeedType(seedStockTb.getSeedType());
+            seed.setHarvestType(seedStockTb.getHarvestType());
+            seed.setHarvestTime(seedStockTb.getHarvestTime());
+            seed.setSeedNumber(seedStockTb.getSeedNumber());
+            seed.setUnit(seedStockTb.getUnit());
+            seed.setSourceType(seedStockTb.getSourceType());
+            seed.setProductionLocationName(seedStockTb.getProductionLocationName());
+            seed.setStockLocationNum(seedStockTb.getStockLocationNum());
+            seed.setSubmitUserId(seedStockTb.getSubmitUserId());
+            seed.setSubmitUserName(seedStockTb.getSubmitUserName());
+            seed.setCreateTime(seedStockTb.getCreateTime());
+            seed.setUpdateTime(seedStockTb.getUpdateTime());
+            seed.setRemarks(seedStockTb.getRemarks());
+            seed.setTotalNumber(seedStockTb.getTotalNumber());
+            seed.setGeneticCharacter(seedStockTb.getGeneticCharacter());
+            seed.setAliasName(seedStockTb.getAliasName());
+            seed.setGeneType(seedStockTb.getGeneType());
+            seed.setMaterialType(seedStockTb.getMaterialType());
+            scanCodeSeedRspDTO.getSeedList().add(seed);
         }
-        CerTransformTb cerTransformTb = cerTransformTbMapper.selectOneByTransformCodeAndVectorTaskCode(transformUniqueCodeDTO.getTransformCode(), transformUniqueCodeDTO.getVectorTaskCode());
-        if (cerTransformTb == null) {
-            throw new BusinessException("转化编号非法：" + cerTransformTb.getTransformCode());
-        }
-        CerVectorGroupTb cerVectorGroupTb = cerVectorGroupTbMapper.selectOneByGroupNameAndVectorTaskId(cerTransformTb.getPlasmidName(), cerVectorTaskTb.getId());
-        CerProjectTb cerProjectTb = cerProjectTbMapper.selectOneByProjectCode(cerVectorTaskTb.getProjectCode());
-        CerSubProjectTb cerSubProjectTb = cerSubProjectTbMapper.selectOneBySubProjectCode(cerVectorTaskTb.getSubProjectCode());
 
-        ScanCodeTransformRspDTO scanCodeTransformRspDTO = new ScanCodeTransformRspDTO();
-        scanCodeTransformRspDTO.setProjectCode(cerVectorTaskTb.getProjectCode());
-        scanCodeTransformRspDTO.setProjectName(cerProjectTb.getProjectName());
-        scanCodeTransformRspDTO.setSubProjectCode(cerSubProjectTb.getSubProjectCode());
-        scanCodeTransformRspDTO.setSubProjectName(cerSubProjectTb.getSubProjectName());
-        scanCodeTransformRspDTO.setVectorTaskCode(cerVectorTaskTb.getVectorTaskCode());
-        scanCodeTransformRspDTO.setVectorTaskName(cerVectorTaskTb.getVectorTaskName());
-        ScanCodeTransformRspDTO.Transform transform = BeanUtil.copyProperties(cerTransformTb, ScanCodeTransformRspDTO.Transform.class);
-        transform.setTransformName(cerVectorGroupTb.getGroupName());
-        transform.setPlasmidName(cerVectorGroupTb.getPlasmidNames());
-        scanCodeTransformRspDTO.setTransform(transform);
-        return scanCodeTransformRspDTO;
+        return scanCodeSeedRspDTO;
+    }
+
+    private List<SeedStockTb> findSeed(String seedNum, List<SeedStockTb> seedStockTbList) {
+        if (seedNum == null) {
+            return seedStockTbList;
+        }
+        SeedStockTb seedStockTb = seedStockTbMapper.selectOneBySeedNum(seedNum);
+        if (seedStockTb != null) {
+            seedStockTbList.add(seedStockTb);
+            findSeed(seedNum, seedStockTbList);
+        } else {
+            return seedStockTbList;
+        }
+        return seedStockTbList;
     }
 
 

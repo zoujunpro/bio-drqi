a5363c675e55a07fd0295ce927df20aa30550cb6
zoujun <13683366156@163.com>
2024-11-25 10:40:03 +0800

    11

--- bio-drqi-applet/src/main/java/com/bio/drqi/applet/service/impl/LoginServiceImpl.java
+++ bio-drqi-applet/src/main/java/com/bio/drqi/applet/service/impl/LoginServiceImpl.java
@@ -55,7 +55,6 @@ public class LoginServiceImpl implements LoginService {
             log.error("微信接口调用失败 ", e);
             throw new BusinessException("微信接口调用失败，请联系系统开发人员");
         }
-
         ResponseResult<UserDetailRspDTO> responseResult = remoteUserService.queryUserByTelephone(bioAppletLoginTb.getTelephone());
         if (responseResult.isError()) {
             throw new BusinessException("用户服务调用异常");

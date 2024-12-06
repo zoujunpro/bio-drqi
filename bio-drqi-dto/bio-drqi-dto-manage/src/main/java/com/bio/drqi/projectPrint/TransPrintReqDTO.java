90b52f1b0b956b6b3c30cfee49f5377ffe188599
zoujun <13683366156@163.com>
2024-12-06 09:56:58 +0800

    11

--- bio-drqi-dto/bio-drqi-dto-manage/src/main/java/com/bio/drqi/projectPrint/TransPrintReqDTO.java
+++ bio-drqi-dto/bio-drqi-dto-manage/src/main/java/com/bio/drqi/projectPrint/TransPrintReqDTO.java
@@ -1,13 +1,15 @@
 package com.bio.drqi.projectPrint;
 
+import cn.hutool.json.JSONUtil;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TransPrintReqDTO {
 
-    private List<Content> contentList;
+    private List<Content> contentList=new ArrayList<>();
 
     @Data
     public static class Content {
@@ -15,5 +17,16 @@ public class TransPrintReqDTO {
         private String vectorTaskCode;
 
         private String plantCode;
+    }
+
+    public static void main(String[] args) {
+        TransPrintReqDTO transPrintReqDTO=new TransPrintReqDTO();
+        Content content=new Content();
+        content.setVectorTaskCode("xx01101-05");
+        content.setPlantCode("AG10-01");
+        transPrintReqDTO.getContentList().add(content);
+        System.out.println(JSONUtil.toJsonStr(transPrintReqDTO));
+
+
     }
 }

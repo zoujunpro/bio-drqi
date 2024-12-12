a56eb2186505472e0529d18da310c68709a38e64
zoujun <13683366156@163.com>
2024-12-12 13:39:55 +0800

    11

--- bio-drqi-applet/src/main/java/com/bio/drqi/applet/service/codescan/template/T0PlantCodeScanService.java
+++ bio-drqi-applet/src/main/java/com/bio/drqi/applet/service/codescan/template/T0PlantCodeScanService.java
@@ -63,7 +63,7 @@ public class T0PlantCodeScanService extends AbstractBaseCodeScanService<PlantUni
         scanCodeT0PlantTestRspDTO.setVectorTaskCode(cerVectorTaskTb.getVectorTaskCode());
         scanCodeT0PlantTestRspDTO.setVectorTaskName(cerVectorTaskTb.getVectorTaskName());
         scanCodeT0PlantTestRspDTO.setPlantDtlInfo(BeanUtils.copyProperties(cerPlantDtlTb, ScanCodeT0PlantTestRspDTO.PlantDtlInfo.class));
-        scanCodeT0PlantTestRspDTO.getPlantDtlInfo().setGeneration(GenerationEnum.getGenerationDesc(scanCodeT0PlantTestRspDTO.getPlantDtlInfo().getGeneration()));
+        scanCodeT0PlantTestRspDTO.getPlantDtlInfo().setGeneration(GenerationEnum.getGenerationDesc(scanCodeT0PlantTestRspDTO.getPlantDtlInfo().getGeneration())) ;
         return scanCodeT0PlantTestRspDTO;
     }
 }

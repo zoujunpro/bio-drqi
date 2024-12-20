c5622517e9ee0775537d938e76e05e21c4885a1b
zoujun <13683366156@163.com>
2024-12-20 09:15:07 +0800

    11

--- bio-drqi-manage/bio-drqi-manage-main/src/main/java/com/bio/drqi/manage/service/task/project/NewSampleTestProcService.java
+++ bio-drqi-manage/bio-drqi-manage-main/src/main/java/com/bio/drqi/manage/service/task/project/NewSampleTestProcService.java
@@ -98,7 +98,7 @@ public class NewSampleTestProcService extends AbstractBaseProjectTaskService {
                     if (oneTestType.equals(newSampleTestDTO.getTestType())) {
                         List<CerSampleTestTb> cerSampleTestTbList = cerSampleTestTbMapper.selectAllByApplyNo(bioTaskDtlTb.getTaskNum());
                         LayoutConfirmReqDTO layoutConfirmReqDTO = new LayoutConfirmReqDTO();
-                        layoutConfirmReqDTO.setApplyNo(cerSampleApplyTb.getApplyNo());
+                        layoutConfirmReqDTO.setApplyNo(bioTaskDtlTb.getTaskNum());
                         for (CerSampleTestTb cerSampleTestTb:cerSampleTestTbList){
                             layoutConfirmReqDTO.fillSampleToSingleList(cerSampleTestTb.getVectorTaskCode(),cerSampleTestTb.getTransformCode(),cerSampleTestTb.getSampleCode(),cerSampleTestTb.getIdentifyPrimer());
                         }

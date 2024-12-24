1131f4a218c43cc31520f795413883ec3c1b686a
zoujun <13683366156@163.com>
2024-12-24 17:04:31 +0800

    11

---
+++ bio-drqi-dao/src/main/java/com/bio/drqi/domain/CerSampleTestBioResultRef.java
@@ -0,0 +1,59 @@
+package com.bio.drqi.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 
+ * @TableName cer_sample_test_bio_result_ref
+ */
+@TableName(value ="cer_sample_test_bio_result_ref")
+@Data
+public class CerSampleTestBioResultRef implements Serializable {
+    /**
+     * 主键ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 申请编号
+     */
+    private String applyNo;
+
+    /**
+     * 实施方案编号
+     */
+    private String sampleCode;
+
+    /**
+     * 取样编号
+     */
+    private String vectorTaskCode;
+
+    /**
+     * 材料名称
+     */
+    private String sampleId;
+
+    /**
+     * 测序编号
+     */
+    private String runId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+
+}
---
+++ bio-drqi-dao/src/main/java/com/bio/drqi/service/CerSampleTestBioResultRefService.java
@@ -0,0 +1,13 @@
+package com.bio.drqi.service;
+
+import com.bio.drqi.domain.CerSampleTestBioResultRef;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author zou'jun
+* @description 针对表【cer_sample_test_bio_result_ref】的数据库操作Service
+* @createDate 2024-12-24 15:13:05
+*/
+public interface CerSampleTestBioResultRefService extends IService<CerSampleTestBioResultRef> {
+
+}

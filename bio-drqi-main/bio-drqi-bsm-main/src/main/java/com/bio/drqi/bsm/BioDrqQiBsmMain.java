a26e282acf85098b7ff903318ace6cfce8ffee7e
zoujun <13683366156@163.com>
2025-02-27 09:43:20 +0800

    供应链管理项目初始化

---
+++ bio-drqi-main/bio-drqi-bsm-main/src/main/java/com/bio/drqi/bsm/BioDrqQiBsmMain.java
@@ -0,0 +1,16 @@
+package com.bio.drqi.bsm;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(scanBasePackages = {"com.bio.drqi","com.bio.common","com.bio.print","com.bio.base","com.bio.flow","com.bio.core"})
+@EnableDiscoveryClient
+@MapperScan(basePackages = "com.bio.drqi.mapper")
+@EnableFeignClients(basePackages = {"com.bio.print.api","com.bio.base.api","com.bio.drqi.external.client"})
+@EnableScheduling
+public class BioDrqQiBsmMain {
+
+}

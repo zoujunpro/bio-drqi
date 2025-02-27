c2d9cf9f97b47bd9fda3e05cc68e08fd75a43644
zoujun <13683366156@163.com>
2025-02-27 17:19:56 +0800

    供应链管理项目初始化

--- bio-drqi-bsm/src/main/java/com/bio/drqi/bsm/controller/BmsBrandController.java
+++ bio-drqi-bsm/src/main/java/com/bio/drqi/bsm/controller/BmsBrandController.java
@@ -1,7 +1,19 @@
 package com.bio.drqi.bsm.controller;
 
+import com.bio.common.core.dto.ResponseResult;
+import com.bio.common.web.aspect.WebLog;
+import com.bio.drqi.bsm.dto.req.BmsBrandAddReqDTO;
+import com.bio.drqi.bsm.dto.req.BmsBrandEditReqDTO;
+import com.bio.drqi.bsm.dto.req.BmsBrandListPageReqDTO;
+import com.bio.drqi.bsm.dto.rsp.BmsBrandListAllRspDTO;
+import com.bio.drqi.bsm.dto.rsp.BmsBrandListPageRspDTO;
+import com.github.pagehelper.PageInfo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 品牌管理
@@ -9,4 +21,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/brand")
 public class BmsBrandController {
+
+    /**
+     * 品牌管理-分页查询
+     * @param bmsBrandListPageReqDTO
+     * @return
+     */
+    @PostMapping("/listPage")
+    @WebLog(desc = "品牌管理-分页查询")
+    public ResponseResult<PageInfo<BmsBrandListPageRspDTO>> listPage(BmsBrandListPageReqDTO bmsBrandListPageReqDTO){
+        return null;
+    }
+
+
+    /**
+     * 品牌管理-查询所有
+     * @return
+     */
+    @GetMapping("/listAll")
+    @WebLog(desc = "品牌管理-查询所有")
+    public ResponseResult<List<BmsBrandListAllRspDTO>> listAll(){
+        return null;
+    }
+
+    /**
+     * 品牌管理-新增
+     * @return
+     */
+    @PostMapping("/add")
+    @WebLog(desc = "品牌管理-新增")
+    public ResponseResult<String> add(BmsBrandAddReqDTO bmsBrandAddReqDTO){
+        return null;
+    }
+
+    /**
+     * 品牌管理-删除
+     * @return
+     */
+    @GetMapping("/delete")
+    @WebLog(desc = "品牌管理-删除")
+    public ResponseResult<String> delete(){
+        return null;
+    }
+
+    /**
+     * 品牌管理-修改
+     * @return
+     */
+    @PostMapping("/edit")
+    @WebLog(desc = "品牌管理-修改")
+    public ResponseResult<String> edit(BmsBrandEditReqDTO bmsBrandEditReqDTO){
+        return null;
+    }
 }
--- bio-drqi-bsm/src/main/java/com/bio/drqi/bsm/controller/BmsSupplierController.java
+++ bio-drqi-bsm/src/main/java/com/bio/drqi/bsm/controller/BmsSupplierController.java
@@ -4,9 +4,12 @@ import com.bio.common.core.dto.ResponseResult;
 import com.bio.common.web.aspect.WebLog;
 import com.bio.drqi.bsm.dto.req.BmsSupplierAddReqDTO;
 import com.bio.drqi.bsm.dto.req.BmsSupplierListPageReqDTO;
+import com.bio.drqi.bsm.dto.rsp.BmsSupplierListAllRspDTO;
 import com.bio.drqi.bsm.dto.rsp.BmsSupplierListPageRspDTO;
 import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 供应商管理
@@ -18,33 +21,68 @@ public class BmsSupplierController {
 
     /**
      * 供应商管理-分页查询
+     *
      * @param bmsSupplierListPageReqDTO
      * @return
      */
     @PostMapping("/listPage")
     @WebLog(desc = "供应商管理-分页查询")
-    public ResponseResult<PageInfo<BmsSupplierListPageRspDTO>> listPage(@RequestBody BmsSupplierListPageReqDTO bmsSupplierListPageReqDTO){
-            return null;
+    public ResponseResult<PageInfo<BmsSupplierListPageRspDTO>> listPage(@RequestBody BmsSupplierListPageReqDTO bmsSupplierListPageReqDTO) {
+        return null;
+    }
+    /**
+     * 供应商管理-查询全部
+     *
+     * @return
+     */
+    @GetMapping("/listALl")
+    @WebLog(desc = "供应商管理-查询全部")
+    public ResponseResult<List<BmsSupplierListAllRspDTO>> listALl() {
+        return null;
     }
 
     /**
      * 供应商管理-新增
+     *
      * @param bmsSupplierAddReqDTO
      * @return
      */
     @PostMapping("/add")
     @WebLog(desc = "供应商管理-新增")
-    public ResponseResult<String> add(@RequestBody BmsSupplierAddReqDTO bmsSupplierAddReqDTO){
+    public ResponseResult<String> add(@RequestBody BmsSupplierAddReqDTO bmsSupplierAddReqDTO) {
         return null;
     }
+
     /**
      * 供应商管理-删除
+     *
      * @param id
      * @return
      */
     @GetMapping("/delete")
     @WebLog(desc = "供应商管理-删除")
-    public ResponseResult<String> delete(@RequestParam Integer id){
+    public ResponseResult<String> delete(@RequestParam Integer id) {
         return null;
+    }
+
+    /**
+     * 供应商管理-导出
+     *
+     * @return
+     */
+    @GetMapping("/exportExcel")
+    @WebLog(desc = "供应商管理-导出")
+    public void exportExcel() {
+    }
+
+    /**
+     * 供应商管理-导入
+     *
+     * @return
+     */
+    @GetMapping("/exportExcel")
+    @WebLog(desc = "供应商管理-导入")
+    public void importExcel() {
+
     }
 }

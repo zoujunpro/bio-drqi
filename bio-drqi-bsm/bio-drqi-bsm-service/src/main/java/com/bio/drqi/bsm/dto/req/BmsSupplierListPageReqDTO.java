01f239056bc6bfed0b91c9197d97c791fc7433f0
zoujun <13683366156@163.com>
2025-02-28 16:17:36 +0800

    供应链管理项目初始化

---
+++ bio-drqi-bsm/bio-drqi-bsm-service/src/main/java/com/bio/drqi/bsm/dto/req/BmsSupplierListPageReqDTO.java
@@ -0,0 +1,8 @@
+package com.bio.drqi.bsm.dto.req;
+
+import com.bio.drqi.base.PageDTO;
+import lombok.Data;
+
+@Data
+public class BmsSupplierListPageReqDTO extends PageDTO {
+}
--- bio-drqi-bsm/src/main/java/com/bio/drqi/bsm/dto/req/BmsSupplierListPageReqDTO.java
+++
@@ -1,7 +0,0 @@
-package com.bio.drqi.bsm.dto.req;
-
-import lombok.Data;
-
-@Data
-public class BmsSupplierListPageReqDTO {
-}

428edfb1d9645f60a30af4facdcbc3540127c9ce
zoujun <13683366156@163.com>
2025-03-11 14:19:05 +0800

    供应链管理项目初始化

--- bio-drqi-bsm/bio-drqi-bsm-dto/src/main/java/com/bio/drqi/bsm/rsp/BmsProductQueryListRspDTO.java
+++ bio-drqi-bsm/bio-drqi-bsm-dto/src/main/java/com/bio/drqi/bsm/rsp/BmsProductQueryListRspDTO.java
@@ -2,8 +2,12 @@ package com.bio.drqi.bsm.rsp;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class BmsProductQueryListRspDTO {
+    private Integer id;
+
     /**
      * 商品名称
      */
@@ -17,5 +21,55 @@ public class BmsProductQueryListRspDTO {
     /**
      * 商品内部编号
      */
-    private String projectInnerCode;
+    private String productInnerCode;
+
+    /**
+     * 商品类别编号
+     */
+    private String productCategoryCode;
+
+    /**
+     * 商品类型编号
+     */
+    private String productTypeCode;
+
+    /**
+     * 供应商编号
+     */
+    private String supplierCode;
+
+    /**
+     * 品牌名称
+     */
+    private String brandName;
+
+    /**
+     * 品牌编号
+     */
+    private String brandCode;
+
+    /**
+     * 商品规格
+     */
+    private String productSpecs;
+
+    /**
+     * 创建日期
+     */
+    private Date createTime;
+
+    /**
+     * 创建人ID
+     */
+    private Integer createUserId;
+
+    /**
+     * 创建人名称
+     */
+    private String createUserName;
+
+    /**
+     * 删除标识
+     */
+    private String deleteFlag;
 }

6b01e3da1802b0a639ca75f39eea49ba291dded2
zoujun <13683366156@163.com>
2025-03-11 10:02:58 +0800

    供应链管理项目初始化

--- bio-drqi-bsm/bio-drqi-bsm-admin/src/main/java/com/bio/drqi/bsm/controller/BmsProductController.java
+++ bio-drqi-bsm/bio-drqi-bsm-admin/src/main/java/com/bio/drqi/bsm/controller/BmsProductController.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
- * 商品管理
+ * 材料管理
  */
 @RestController
 @RequestMapping("/product")
@@ -25,47 +25,47 @@ public class BmsProductController {
     private BmsProductService bmsProductService;
 
     /**
-     * 商品管理-分页查询
+     * 材料管理-分页查询
      *
      * @param bmsProductListPageReqDTO
      * @return
      */
     @PostMapping("/listPage")
-    @WebLog(desc = "商品管理-分页查询")
+    @WebLog(desc = "材料管理-分页查询")
     public ResponseResult<PageInfo<BmsProductListPageRspDTO>> listPage(@RequestBody @Validated BmsProductListPageReqDTO bmsProductListPageReqDTO) {
         return ResponseResult.getSuccess(bmsProductService.listPage(bmsProductListPageReqDTO));
     }
 
     /**
-     * 商品管理-查询
+     * 材料管理-查询
      *
      * @return
      */
     @PostMapping("/queryList")
-    @WebLog(desc = "商品管理-查询")
+    @WebLog(desc = "材料管理-查询")
     public ResponseResult<List<BmsProductQueryListRspDTO>> queryList(@RequestBody @Validated BmsProductQueryListReqDTO bmsProductQueryListReqDTO) {
         return ResponseResult.getSuccess(bmsProductService.queryList(bmsProductQueryListReqDTO));
     }
 
     /**
-     * 商品管理-导出全部
+     * 材料管理-导出全部
      *
      * @return
      */
     @PostMapping("/exportExcel")
-    @WebLog(desc = "商品管理-导出全部")
+    @WebLog(desc = "材料管理-导出全部")
     public void exportExcel(@RequestBody @Validated BmsProductExportExcelReqDTO bmsProductExportExcelReqDTO) {
         bmsProductService.exportExcel(bmsProductExportExcelReqDTO);
     }
 
 
     /**
-     * 商品管理-添加
+     * 材料管理-添加
      *
      * @return
      */
     @PostMapping("/add")
-    @WebLog(desc = "商品管理-添加")
+    @WebLog(desc = "材料管理-添加")
     public ResponseResult<String> add(@RequestBody @Validated BmsProductAddReqDTO bmsProductAddReqDTO) {
         bmsProductService.add(bmsProductAddReqDTO);
         return ResponseResult.getSuccess("ok");
@@ -73,12 +73,12 @@ public class BmsProductController {
 
 
     /**
-     * 商品管理-删除
+     * 材料管理-删除
      *
      * @return
      */
     @GetMapping("/delete")
-    @WebLog(desc = "商品管理-删除")
+    @WebLog(desc = "材料管理-删除")
     public ResponseResult<String> delete(@RequestParam @Validated @NotNull Integer id) {
         bmsProductService.delete(id);
         return ResponseResult.getSuccess("ok");
@@ -86,12 +86,12 @@ public class BmsProductController {
 
 
     /**
-     * 商品管理-编辑
+     * 材料管理-编辑
      *
      * @return
      */
     @PostMapping("/edit")
-    @WebLog(desc = "商品管理-编辑")
+    @WebLog(desc = "材料管理-编辑")
     public ResponseResult<String> edit(@RequestBody @Validated BmsProductEditReqDTO bmsProductEditReqDTO) {
         bmsProductService.edit(bmsProductEditReqDTO);
         return ResponseResult.getSuccess("ok");

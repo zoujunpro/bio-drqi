684791fcaf0b7ca946e2b1c7db9bd93d01d68114
zoujun <13683366156@163.com>
2025-03-12 19:28:16 +0800

    供应链管理项目初始化

--- bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/project/req/ProjectListReqDTO.java
+++ bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/project/req/ProjectListReqDTO.java
@@ -1,7 +1,7 @@
 package com.bio.drqi.manage.project.req;
 
+import com.bio.common.core.util.EnumValue;
 import com.bio.drqi.manage.base.PageDTO;
-import com.bio.drqi.manage.validator.EnumValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;

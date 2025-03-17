e539ba515f18dd972b0b4108a1762ca8191cdf63
zoujun <13683366156@163.com>
2025-03-17 09:42:53 +0800

    供应链管理项目初始化

--- bio-drqi-bsm/bio-drqi-bsm-admin/src/main/java/com/bio/drqi/bsm/controller/BmsOrderDetailController.java
+++ bio-drqi-bsm/bio-drqi-bsm-admin/src/main/java/com/bio/drqi/bsm/controller/BmsOrderDetailController.java
@@ -2,8 +2,10 @@ package com.bio.drqi.bsm.controller;
 
 import com.bio.common.core.dto.ResponseResult;
 import com.bio.common.web.aspect.WebLog;
+import com.bio.drqi.bsm.rsp.BmsOrderDetailListPageRspDTO;
 import com.bio.drqi.bsm.rsp.BmsOrderDetailQueryByOrderNumRspDTO;
 import com.bio.drqi.bsm.service.BmsOrderDetailService;
+import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,13 +21,22 @@ public class BmsOrderDetailController {
     @Resource
     private BmsOrderDetailService bmsOrderDetailService;
 
+
+    @PostMapping("listPage")
+    @WebLog(desc = "采购订单明细管理-分页查询")
+    public ResponseResult<PageInfo<BmsOrderDetailListPageRspDTO>> listPage(@RequestBody BmsOrderDetailListPageRspDTO bmsOrderDetailListPageRspDTO) {
+        return null;
+    }
+
+
     /**
      * 采购订单明细管理-订单号查询
+     *
      * @return
      */
     @GetMapping("/queryLByOrderNum")
     @WebLog(desc = "采购订单明细管理-订单号查询")
-    public ResponseResult<List<BmsOrderDetailQueryByOrderNumRspDTO>> queryByOrderNum(@RequestParam String orderNum){
+    public ResponseResult<List<BmsOrderDetailQueryByOrderNumRspDTO>> queryByOrderNum(@RequestParam String orderNum) {
 
         return ResponseResult.getSuccess(bmsOrderDetailService.queryByOrderNum(orderNum));
     }
---
+++ bio-drqi-bsm/bio-drqi-bsm-dto/src/main/java/com/bio/drqi/bsm/rsp/BmsOrderDetailListPageRspDTO.java
@@ -0,0 +1,4 @@
+package com.bio.drqi.bsm.rsp;
+
+public class BmsOrderDetailListPageRspDTO {
+}

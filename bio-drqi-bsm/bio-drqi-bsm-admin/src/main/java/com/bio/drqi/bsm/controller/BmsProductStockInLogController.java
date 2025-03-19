6ec835f80dfc69417d962eb695389e5a49707ba5
zoujun <13683366156@163.com>
2025-03-19 17:55:05 +0800

    供应链管理项目初始化

--- bio-drqi-bsm/bio-drqi-bsm-admin/src/main/java/com/bio/drqi/bsm/controller/BmsProductStockInLogController.java
+++ bio-drqi-bsm/bio-drqi-bsm-admin/src/main/java/com/bio/drqi/bsm/controller/BmsProductStockInLogController.java
@@ -4,8 +4,11 @@ import com.bio.common.core.dto.ResponseResult;
 import com.bio.drqi.bsm.req.BmsProductStockInLogListPageReqDTO;
 import com.bio.drqi.bsm.rsp.BmsProductStockInLogDetailRspDTO;
 import com.bio.drqi.bsm.rsp.BmsProductStockInLogListPageRspDTO;
+import com.bio.drqi.bsm.service.BmsProductStockInService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * 入库存明细管理
@@ -15,21 +18,28 @@ import org.springframework.web.bind.annotation.*;
 public class BmsProductStockInLogController {
 
 
+    @Resource
+    private BmsProductStockInService bmsProductStockInService;
+
     /**
-     *入库存明细管理-分页查询
+     * 入库存明细管理-分页查询
+     *
      * @param bmsProductStockInLogListPageReqDTO
      * @return
      */
     @PostMapping("/listPage")
-    public ResponseResult<PageInfo<BmsProductStockInLogListPageRspDTO>> listPage(@RequestBody BmsProductStockInLogListPageReqDTO bmsProductStockInLogListPageReqDTO){
-
-        return null;
+    public ResponseResult<PageInfo<BmsProductStockInLogListPageRspDTO>> listPage(@RequestBody BmsProductStockInLogListPageReqDTO bmsProductStockInLogListPageReqDTO) {
+        return ResponseResult.getSuccess(bmsProductStockInService.listPage(bmsProductStockInLogListPageReqDTO));
     }
 
+    /**
+     * 入库存明细管理-详情
+     *
+     * @return
+     */
     @GetMapping("/detail")
-    public ResponseResult<BmsProductStockInLogDetailRspDTO> detail(){
-
-        return null;
+    public ResponseResult<BmsProductStockInLogDetailRspDTO> detail(@RequestParam Integer id) {
+        return ResponseResult.getSuccess(bmsProductStockInService.detail(id));
     }
 
 }
---
+++ bio-drqi-bsm/bio-drqi-bsm-service/src/main/java/com/bio/drqi/bsm/service/BmsProductStockInService.java
@@ -0,0 +1,21 @@
+package com.bio.drqi.bsm.service;
+
+import com.bio.drqi.bsm.req.BmsProductStockInLogListPageReqDTO;
+import com.bio.drqi.bsm.rsp.BmsProductStockInLogDetailRspDTO;
+import com.bio.drqi.bsm.rsp.BmsProductStockInLogListPageRspDTO;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface BmsProductStockInService {
+
+    PageInfo<BmsProductStockInLogListPageRspDTO> listPage(BmsProductStockInLogListPageReqDTO bmsProductStockInLogListPageReqDTO);
+
+    /**
+     * 入库存明细管理-详情
+     *
+     * @return
+     */
+    BmsProductStockInLogDetailRspDTO detail(Integer id);
+
+}

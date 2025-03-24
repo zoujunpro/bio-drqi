8175576f42104d165f18fbcca57883f0cf87676a
zoujun <13683366156@163.com>
2025-03-24 16:56:44 +0800

    供应链管理项目初始化

--- bio-drqi-bsm/bio-drqi-bsm-dto/src/main/java/com/bio/drqi/bsm/req/BmsProductListPageReqDTO.java
+++ bio-drqi-bsm/bio-drqi-bsm-dto/src/main/java/com/bio/drqi/bsm/req/BmsProductListPageReqDTO.java
@@ -6,6 +6,12 @@ import lombok.Data;
 @Data
 public class BmsProductListPageReqDTO extends PageDTO {
     private String brandCode;
+
     private String productName;
+
     private String deleteFlag;
+    /**
+     * 供应商编号
+     */
+    private String supplierCode;
 }

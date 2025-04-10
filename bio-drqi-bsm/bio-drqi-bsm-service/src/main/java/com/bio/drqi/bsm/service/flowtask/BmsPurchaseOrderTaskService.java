10b7c88a22284309784c7507c465f6a172273ca1
zoujun <13683366156@163.com>
2025-04-10 15:06:09 +0800

    供应链管理项目初始化

--- bio-drqi-bsm/bio-drqi-bsm-service/src/main/java/com/bio/drqi/bsm/service/flowtask/BmsPurchaseOrderTaskService.java
+++ bio-drqi-bsm/bio-drqi-bsm-service/src/main/java/com/bio/drqi/bsm/service/flowtask/BmsPurchaseOrderTaskService.java
@@ -120,7 +120,6 @@ public class BmsPurchaseOrderTaskService extends AbstractBsmBaseTaskService {
                 bmsOrderDetailTb.setOrderNum(bmsOrderTb.getOrderNum());
                 bmsOrderDetailTb.setOrderDetailNum(IdUtils.simpleUUID());
                 bmsOrderDetailTb.setProjectCode(product.getProjectCode());
-                bmsOrderDetailTb.setProjectName(product.getProductName());
                 bmsOrderDetailTb.setSupplierName(product.getSupplierName());
                 bmsOrderDetailTb.setSupplierCode(product.getSupplierCode());
                 bmsOrderDetailTb.setContactUserTelephone(bmsSupplierTb.getContactUserTelephone());

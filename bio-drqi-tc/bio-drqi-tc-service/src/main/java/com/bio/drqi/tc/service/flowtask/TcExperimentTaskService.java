227c10c41c37e9c2f42549e05bd5e676f0c37285
zoujun <13683366156@163.com>
2025-05-15 16:20:05 +0800

    供应链管理项目初始化

--- bio-drqi-tc/bio-drqi-tc-service/src/main/java/com/bio/drqi/tc/service/flowtask/TcExperimentTaskService.java
+++ bio-drqi-tc/bio-drqi-tc-service/src/main/java/com/bio/drqi/tc/service/flowtask/TcExperimentTaskService.java
@@ -6,6 +6,7 @@ import com.bio.common.core.dto.BusinessException;
 import com.bio.common.core.util.ExcelUtil;
 import com.bio.common.core.util.StringUtils;
 import com.bio.common.core.util.ValidatorUtil;
+import com.bio.common.oss.service.OssService;
 import com.bio.drqi.domain.BioTaskDtlTb;
 import com.bio.drqi.domain.TcExperimentDesignTb;
 import com.bio.drqi.domain.TcExperimentTb;
@@ -14,14 +15,17 @@ import com.bio.drqi.mapper.TcExperimentDesignTbMapper;
 import com.bio.drqi.mapper.TcExperimentTbMapper;
 import com.bio.drqi.tc.service.dto.ExperimentDesignExcelDTO;
 import com.bio.drqi.tc.service.dto.TcExperimentTaskDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service("tc_experiment_task_apply")
+@Slf4j
 public class TcExperimentTaskService extends AbstractTcBaseTaskService {
 
 
@@ -30,6 +34,10 @@ public class TcExperimentTaskService extends AbstractTcBaseTaskService {
 
     @Resource
     private TcExperimentDesignTbMapper tcExperimentDesignTbMapper;
+
+
+    @Resource
+    private OssService ossService;
 
     @Override
     public void taskApply(BioTaskDtlTb bioTaskDtlTb) {
@@ -57,11 +65,19 @@ public class TcExperimentTaskService extends AbstractTcBaseTaskService {
             tcExperimentTbMapper.insert(tcExperimentTb);
 
             if(StringUtils.isEmpty(tcExperimentTaskDTO.getExperimentDesignUrl())){
-                throw new BusinessException("大田试验方案缺失");
+                throw new BusinessException("大田试验田间设计缺失");
             }
-            List<ExperimentDesignExcelDTO> experimentDesignExcelDTOList = ExcelUtil.readExcel(tcExperimentTaskDTO.getExperimentDesignUrl(), ExperimentDesignExcelDTO.class);
+
+            String tempFilePath = System.getProperty("java.io.tmpdir") + File.separator + tcExperimentTaskDTO.getExperimentDesignUrl();
+            try {
+                ossService.downloadPath(tempFilePath, tcExperimentTaskDTO.getExperimentDesignUrl());
+            } catch (Exception e) {
+                log.error("【大田试验田间设计】文件从oss下载失败", e);
+                throw new BusinessException("文件处理异常");
+            }
+            List<ExperimentDesignExcelDTO> experimentDesignExcelDTOList = ExcelUtil.readExcel(tempFilePath, ExperimentDesignExcelDTO.class);
             if(CollectionUtil.isEmpty(experimentDesignExcelDTOList)){
-                throw new BusinessException("大田试验方案内容缺失");
+                throw new BusinessException("大田试验田间设计没有具体内容");
             }
             List<TcExperimentDesignTb> tcExperimentDesignTbList = new ArrayList<TcExperimentDesignTb>();
             for (ExperimentDesignExcelDTO experimentDesignExcelDTO:experimentDesignExcelDTOList){

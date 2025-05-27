61361dfeac94ff52413173c1054e64580cb98949
zoujun <13683366156@163.com>
2025-05-27 14:38:02 +0800

    供应链管理项目初始化

--- bio-drqi-tc/bio-drqi-tc-dto/src/main/java/com/bio/drqi/tc/rsp/TcExperimentListAllRspDTO.java
+++ bio-drqi-tc/bio-drqi-tc-dto/src/main/java/com/bio/drqi/tc/rsp/TcExperimentListAllRspDTO.java
@@ -11,4 +11,39 @@ public class TcExperimentListAllRspDTO {
      * 实验编号
      */
     private String experimentNum;
+
+    /**
+     * 物种名称
+     */
+    private String speciesName;
+
+    /**
+     * 上传附件
+     */
+    private String fileUrl;
+
+    /**
+     * 实验目的
+     */
+    private String experimentGoal;
+
+    /**
+     * 实验地点
+     */
+    private String experimentAddress;
+
+    /**
+     * 申请人
+     */
+    private String applyUserName;
+
+    /**
+     * 申请人iD
+     */
+    private Integer applyUserId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
 }

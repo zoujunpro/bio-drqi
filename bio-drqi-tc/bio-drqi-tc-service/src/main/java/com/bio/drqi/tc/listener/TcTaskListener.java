6dc71279fe38e2c4cefc08b3b552892a14776ea4
zoujun <13683366156@163.com>
2025-05-28 10:47:34 +0800

    供应链管理项目初始化

---
+++ bio-drqi-tc/bio-drqi-tc-service/src/main/java/com/bio/drqi/tc/listener/TcTaskListener.java
@@ -0,0 +1,114 @@
+package com.bio.drqi.tc.listener;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
+import com.bio.base.api.RemoteUserService;
+import com.bio.base.user.req.QueryUserByIdListReqDTO;
+import com.bio.base.user.rsp.UserBaseInfoRspDTO;
+import com.bio.common.core.dto.BusinessException;
+import com.bio.common.core.dto.ResponseResult;
+import com.bio.common.core.util.StringUtils;
+import com.bio.drqi.domain.BioTaskDtlTb;
+import com.bio.drqi.feishu.FeiShuService;
+import com.bio.drqi.feishu.dto.Message;
+import com.bio.drqi.mapper.BioTaskDtlTbMapper;
+import com.bio.flow.enums.EventType;
+import com.bio.flow.hander.DefaultDuplicateCopyHandler;
+import com.bio.flow.service.FlowTaskListener;
+import com.easyflow.engine.FlowEngineService;
+import com.easyflow.engine.core.FlowActor;
+import com.easyflow.engine.entity.FlowTaskActorTb;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class TcTaskListener extends DefaultDuplicateCopyHandler implements FlowTaskListener<BioTaskDtlTb> {
+
+    private final static Map<String, String> vieMap = new ConcurrentHashMap<>();
+
+    @Value("${cer.properties.feiShuTcJumpUrl}")
+    private String feiShuTcJumpUrl;
+
+    @Resource
+    private BioTaskDtlTbMapper bioTaskDtlTbMapper;
+
+    @Resource
+    private FlowEngineService flowEngineService;
+
+    @Resource
+    private RemoteUserService remoteUserService;
+
+    @Resource
+    private FeiShuService feiShuService;
+
+
+    static {
+        vieMap.put("tc_experiment_task_apply", "fieldTest");
+        vieMap.put("tc_sample_test_task_apply", "fieldSample");
+        vieMap.put("tc_pollination_task_apply", "fieldPollination");
+        vieMap.put("tc_harvest_task_apply", "fieldHarvest");
+    }
+
+    @Override
+    public void notice(EventType eventType, Supplier<BioTaskDtlTb> supplier) {
+        BioTaskDtlTb bioTaskDtlTb = supplier.get();
+        if (vieMap.get(bioTaskDtlTb.getTaskTypeCode()) == null) {
+            return;
+        }
+        if (EventType.complete == eventType) {
+            String title = "你的" + bioTaskDtlTb.getTaskTypeName() + "已通过";
+            sendMessage(bioTaskDtlTb, title, Arrays.asList(bioTaskDtlTb.getApplyUserId()));
+        } else if (EventType.revoke == eventType) {
+            String title = "你的" + bioTaskDtlTb.getTaskTypeName() + "已撤销";
+            sendMessage(bioTaskDtlTb, title, Arrays.asList(bioTaskDtlTb.getApplyUserId()));
+        } else if (EventType.reject == eventType) {
+            String title = "你的" + bioTaskDtlTb.getTaskTypeName() + "已拒绝";
+            sendMessage(bioTaskDtlTb, title, Arrays.asList(bioTaskDtlTb.getApplyUserId()));
+        } else if (EventType.active == eventType) {
+            List<FlowTaskActorTb> flowTaskActorTbList = flowEngineService.getQueryService().getActiveTaskActorByInstanceId(bioTaskDtlTb.getInstanceId());
+            String title = "你有一个" + bioTaskDtlTb.getTaskTypeName() + "待审批";
+            sendMessage(bioTaskDtlTb, title, flowTaskActorTbList.stream().map(flowTaskActorTb -> Integer.valueOf(flowTaskActorTb.getActorId())).collect(Collectors.toList()));
+        }
+    }
+
+    private void sendMessage(BioTaskDtlTb bioTaskDtlTb, String title, List<Integer> userIdList) {
+        QueryUserByIdListReqDTO queryUserByIdListReqDTO = new QueryUserByIdListReqDTO();
+        queryUserByIdListReqDTO.setUserIdList(userIdList);
+        ResponseResult<List<UserBaseInfoRspDTO>> responseResult = remoteUserService.queryUserByIdList(queryUserByIdListReqDTO);
+        if (responseResult.isError()) {
+            throw new BusinessException(responseResult.getMessage());
+        }
+        List<UserBaseInfoRspDTO> rspDTOList = responseResult.getData();
+        if (CollectionUtil.isEmpty(rspDTOList)) {
+            return;
+        }
+        List<String> openIdList = rspDTOList.stream().filter(userBaseInfoRspDTO -> StringUtils.isNotEmpty(userBaseInfoRspDTO.getFeiShuUserId())).map(UserBaseInfoRspDTO::getFeiShuUserId).collect(Collectors.toList());
+        String content = "**任务描述：**" + bioTaskDtlTb.getTaskDesc() + "\n" + "**申  请 人：**" + bioTaskDtlTb.getApplyUserName() + "\n" + "**申请时间：**" + DateUtil.format(bioTaskDtlTb.getCreateTime(), DatePattern.NORM_DATETIME_PATTERN);
+        Message message = new Message();
+        message.setTitle(title);
+        message.setContent(content);
+        message.setUrl(String.format(feiShuTcJumpUrl, vieMap.get(bioTaskDtlTb.getTaskTypeCode()), bioTaskDtlTb.getId()));
+        feiShuService.sendCardMessage(openIdList, message);
+    }
+
+    @Override
+    public void doHandle(List<FlowActor> flowActorList, Long instanceId) {
+        BioTaskDtlTb bioTaskDtlTb = bioTaskDtlTbMapper.selectOneByInstanceId(instanceId);
+        if (Objects.nonNull(bioTaskDtlTb)) {
+            String title = bioTaskDtlTb.getTaskTypeName() + "抄送通知";
+            sendMessage(bioTaskDtlTb, title, flowActorList.stream().map(flowActor -> Integer.valueOf(flowActor.getCreateId())).collect(Collectors.toList()));
+        }
+    }
+}

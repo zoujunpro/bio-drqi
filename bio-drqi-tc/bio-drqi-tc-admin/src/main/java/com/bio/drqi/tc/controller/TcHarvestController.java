6e5ef4dea5af60e1cb7835fae0808ac0ea49b18b
zoujun <13683366156@163.com>
2025-05-29 17:40:48 +0800

    供应链管理项目初始化

--- bio-drqi-tc/bio-drqi-tc-admin/src/main/java/com/bio/drqi/tc/controller/TcHarvestController.java
+++ bio-drqi-tc/bio-drqi-tc-admin/src/main/java/com/bio/drqi/tc/controller/TcHarvestController.java
@@ -3,8 +3,6 @@ package com.bio.drqi.tc.controller;
 import com.bio.common.core.dto.ResponseResult;
 import com.bio.common.web.aspect.WebLog;
 import com.bio.drqi.tc.req.TcHarvestCreateHarvestExcelReqDTO;
-import com.bio.drqi.tc.req.TcHarvestListPageReqDTO;
-import com.bio.drqi.tc.rsp.TcHarvestListPageRspDTO;
 import com.bio.drqi.tc.service.TcHarvestService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.validation.annotation.Validated;
@@ -26,10 +24,6 @@ public class TcHarvestController {
     @Resource
     private TcHarvestService tcHarvestService;
 
-
-    public ResponseResult<PageInfo<TcHarvestListPageRspDTO>> listPage(@RequestBody @Validated TcHarvestListPageReqDTO TcHarvestListPageReqDTO) {
-        return ResponseResult.getSuccess(null);
-    }
 
 
     /**

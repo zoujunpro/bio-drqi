ce27972035778eae187b9d18659bb4ef3285425f
zoujun <13683366156@163.com>
2025-06-10 15:44:39 +0800

    供应链管理项目初始化

---
+++ bio-drqi-tc/bio-drqi-tc-service/src/main/java/com/bio/drqi/tc/service/dto/TeTestTaskDTO.java
@@ -0,0 +1,14 @@
+package com.bio.drqi.tc.service.dto;
+
+import lombok.Data;
+
+@Data
+public class TeTestTaskDTO {
+
+    private String fileUrl;
+
+    /**
+     * 实验编号
+     */
+    private String experimentNum;
+}
---
+++ bio-drqi-tc/bio-drqi-tc-service/src/main/java/com/bio/drqi/tc/service/flowtask/TcTestTaskService.java
@@ -0,0 +1,23 @@
+package com.bio.drqi.tc.service.flowtask;
+
+import com.bio.drqi.domain.BioTaskDtlTb;
+import org.springframework.stereotype.Service;
+
+@Service("tc_test_task_apply")
+public class TcTestTaskService  extends AbstractTcBaseTaskService{
+    @Override
+    public void taskApply(BioTaskDtlTb bioTaskDtlTb) {
+
+
+    }
+
+    @Override
+    public void executeTask(BioTaskDtlTb bioTaskDtlTb) {
+
+    }
+
+    @Override
+    public void cancelTask(BioTaskDtlTb bioTaskDtlTb) {
+
+    }
+}

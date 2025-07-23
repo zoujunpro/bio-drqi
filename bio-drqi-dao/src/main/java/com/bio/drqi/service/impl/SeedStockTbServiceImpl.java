f078a630eaaf3e873a6ea506e2a956f15038b53e
zoujun <13683366156@163.com>
2025-07-23 13:54:45 +0800

    供应链管理项目初始化

---
+++ bio-drqi-dao/src/main/java/com/bio/drqi/service/impl/SeedStockTbServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bio.drqi.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.bio.drqi.domain.SeedStockTb;
+import com.bio.drqi.service.SeedStockTbService;
+import com.bio.drqi.mapper.SeedStockTbMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author zou'jun
+* @description 针对表【seed_stock_tb】的数据库操作Service实现
+* @createDate 2025-07-21 16:28:42
+*/
+@Service
+public class SeedStockTbServiceImpl extends ServiceImpl<SeedStockTbMapper, SeedStockTb>
+    implements SeedStockTbService{
+
+}
+
+
+
+

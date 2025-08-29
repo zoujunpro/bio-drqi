eda3676c5ffbed4d72a62d9103d585c05e937891
zoujun <13683366156@163.com>
2025-08-29 10:49:26 +0800

    供应链管理项目初始化

--- bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/sample/req/SampleTestListDetailReqDTO.java
+++ bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/sample/req/SampleTestListDetailReqDTO.java
@@ -9,16 +9,10 @@ public class SampleTestListDetailReqDTO extends PageDTO {
 
     private String applyNo;
 
-    private Integer vectorTaskId;
-
     /**
      * 项目编码
      */
     private String projectCode;
-    /**
-     * 受体材料
-     */
-    private String acceptorMaterial;
 
     /**
      * 子项目编码
@@ -29,6 +23,19 @@ public class SampleTestListDetailReqDTO extends PageDTO {
      * 载体任务编码
      */
     private String vectorTaskCode;
+
+
+    /**
+     * 转化编号/种子编号
+     */
+    private String transformCode;
+
+    /**
+     * 取样编号
+     */
+    private String sampleCode;
+
+
 
 
 }

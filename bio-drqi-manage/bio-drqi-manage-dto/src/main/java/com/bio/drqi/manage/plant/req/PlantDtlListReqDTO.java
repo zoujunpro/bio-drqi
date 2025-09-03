69d80a39d0a9ea7f09d47d089bb3f884bcae4df2
zoujun <13683366156@163.com>
2025-09-03 16:13:07 +0800

    供应链管理项目初始化

--- bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/plant/req/PlantDtlListReqDTO.java
+++ bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/plant/req/PlantDtlListReqDTO.java
@@ -1,9 +1,45 @@
 package com.bio.drqi.manage.plant.req;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.bio.drqi.manage.base.PageDTO;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class PlantDtlListReqDTO extends PageDTO {
+
+    /**
+     * 任务编号
+     */
+    private String taskNum;
+
+    /**
+     * 所属项目编码
+     */
+    private String projectCode;
+
+    /**
+     * 子项目编号
+     */
+    private String subProjectCode;
+
+    /**
+     * 任务编码
+     */
+    private String vectorTaskCode;
+
+    /**
+     * 取样编号
+     */
+    private String sampleCode;
+
+    /**
+     * 项目物种
+     */
+    private String speciesCode;
+
+
 
 }

e6ce41c9d18f88f28bf7bb2ba530203dd904fef9
zoujun <13683366156@163.com>
2025-09-19 11:21:08 +0800

    供应链管理项目初始化

--- bio-drqi-tc/bio-drqi-tc-dto/src/main/java/com/bio/drqi/tc/req/TcPollinationExportPollinationExcelReqDTO.java
+++ bio-drqi-tc/bio-drqi-tc-dto/src/main/java/com/bio/drqi/tc/req/TcPollinationExportPollinationExcelReqDTO.java
@@ -37,6 +37,10 @@ public class TcPollinationExportPollinationExcelReqDTO {
 
         private String motherSampleCode;
 
+        /**
+         * 母本大田单株编号
+         */
+        private String motherTcSampleCode;
 
         /**
          * 母本品种
@@ -63,7 +67,10 @@ public class TcPollinationExportPollinationExcelReqDTO {
          */
         private String fatherSeedNum;
 
-
+        /**
+         * 父本大田单株编号
+         */
+        private String fatherTcSampleCode;
 
         /**
          * 父本单株编号

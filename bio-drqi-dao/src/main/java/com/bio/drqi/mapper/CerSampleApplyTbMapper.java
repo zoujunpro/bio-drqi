287e522c2ce55a5c24939f80016b8a307d060810
zoujun <13683366156@163.com>
2025-10-22 10:09:16 +0800

    供应链管理项目初始化

--- bio-drqi-dao/src/main/java/com/bio/drqi/mapper/CerSampleApplyTbMapper.java
+++ bio-drqi-dao/src/main/java/com/bio/drqi/mapper/CerSampleApplyTbMapper.java
@@ -1,17 +1,21 @@
 package com.bio.drqi.mapper;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 import com.bio.drqi.domain.CerSampleApplyTb;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
 * @author zou'jun
 * @description 针对表【cer_sample_apply_tb(取样检测申请表)】的数据库操作Mapper
-* @createDate 2024-08-28 14:47:51
-* @Entity com.bio.cer.domain.CerSampleApplyTb
+* @createDate 2025-10-22 09:58:22
+* @Entity com.bio.drqi.domain.CerSampleApplyTb
 */
 public interface CerSampleApplyTbMapper extends BaseMapper<CerSampleApplyTb> {
+
+
+    List<CerSampleApplyTb>  selectSelective(CerSampleApplyTb cerSampleApplyTb);
 
     CerSampleApplyTb selectOneByApplyNo(@Param("applyNo") String applyNo);
 
@@ -20,9 +24,6 @@ public interface CerSampleApplyTbMapper extends BaseMapper<CerSampleApplyTb> {
 
 
     int deleteByApplyNo(@Param("applyNo") String applyNo);
-
-
-
 }
 
 
--- bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/sample/req/SampleApplyListPageReqDTO.java
+++ bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/sample/req/SampleApplyListPageReqDTO.java
@@ -3,12 +3,44 @@ package com.bio.drqi.manage.sample.req;
 import com.bio.drqi.manage.base.PageDTO;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class SampleApplyListPageReqDTO extends PageDTO {
 
+    /**
+     * 取样申请编号
+     */
     private String applyNo;
 
-    private String sampleCode;
+
+
+    /**
+     * 取样申请人ID
+     */
+    private Integer applyUserId;
+
+
+    /**
+     * 取样类型 F首次取样   R重复取样
+     */
+    private String applyType;
+
+    /**
+     * 是否是克隆苗取样 Y N
+     */
+    private String cloneFlag;
+
+    /**
+     * 孔板类型 one,more
+     */
+    private String layoutFlag;
+
+    /**
+     * 实施方案编号
+     */
+    private String vectorTaskCode;
+
 
 
 }

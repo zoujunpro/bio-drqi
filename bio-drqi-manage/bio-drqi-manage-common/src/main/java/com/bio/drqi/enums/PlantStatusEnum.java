8a1d1c71a28efce7df803f103f8a4282f38ba4bf
zoujun <13683366156@163.com>
2025-11-11 11:09:56 +0800

    供应链管理项目初始化

--- bio-drqi-manage/bio-drqi-manage-common/src/main/java/com/bio/drqi/enums/PlantStatusEnum.java
+++ bio-drqi-manage/bio-drqi-manage-common/src/main/java/com/bio/drqi/enums/PlantStatusEnum.java
@@ -4,6 +4,7 @@ public enum PlantStatusEnum {
     STATUS_1("1","正常"),
     STATUS_2("2","异常"),
     STATUS_3("3","已剔除"),
+    STATUS_4("4","已收获"),
     ;
     public String code;
     public String desc;

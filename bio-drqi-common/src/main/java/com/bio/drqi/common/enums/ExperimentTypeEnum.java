ded59bf11044b520c8c60980347f9a67fc15718b
zoujun <13683366156@163.com>
2025-11-24 15:44:29 +0800

    供应链管理项目初始化

--- bio-drqi-common/src/main/java/com/bio/drqi/common/enums/ExperimentTypeEnum.java
+++ bio-drqi-common/src/main/java/com/bio/drqi/common/enums/ExperimentTypeEnum.java
@@ -3,8 +3,8 @@ package com.bio.drqi.common.enums;
 public enum ExperimentTypeEnum {
     CODE_1("1", "扩繁"),
     CODE_2("2", "性状测试"),
-    CODE_3("3", "品种审定"),
-    CODE_4("4", "等级试验"),
+    CODE_3("3", "法规试验"),
+    CODE_4("4", "品种审定/等级试验"),
     CODE_5("5", "制种"),
     ;
     public String code;

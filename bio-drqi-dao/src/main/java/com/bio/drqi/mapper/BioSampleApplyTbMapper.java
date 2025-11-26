9c4cead88650c512907eb5daf03439d29229d79d
zoujun <13683366156@163.com>
2025-11-26 10:25:35 +0800

    供应链管理项目初始化

--- bio-drqi-dao/src/main/java/com/bio/drqi/mapper/BioSampleApplyTbMapper.java
+++ bio-drqi-dao/src/main/java/com/bio/drqi/mapper/BioSampleApplyTbMapper.java
@@ -7,8 +7,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 /**
 * @author zou'jun
 * @description 针对表【bio_sample_apply_tb(取样检测申请表)】的数据库操作Mapper
-* @createDate 2025-11-19 11:08:14
-* @Entity com.bio.drqi.domain.PlantSampleApplyTb
+* @createDate 2025-11-26 10:24:53
+* @Entity com.bio.drqi.domain.BioSampleApplyTb
 */
 public interface BioSampleApplyTbMapper extends BaseMapper<BioSampleApplyTb> {
     BioSampleApplyTb selectOneByApplyNo(@Param("applyNo") String applyNo);

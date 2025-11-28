f0ca516e60af8e512bfad3dccd125de5b8dcae71
zoujun <13683366156@163.com>
2025-11-28 16:15:38 +0800

    供应链管理项目初始化

--- bio-drqi-manage/bio-drqi-manage-service/src/main/java/com/bio/drqi/manage/flowtask/project/ConversionAndTransProcServiceBase.java
+++ bio-drqi-manage/bio-drqi-manage-service/src/main/java/com/bio/drqi/manage/flowtask/project/ConversionAndTransProcServiceBase.java
@@ -34,7 +34,7 @@ public class ConversionAndTransProcServiceBase extends AbstractProjectBaseTaskSe
     private CerTransformTbMapper cerTransformTbMapper;
 
     @Resource
-    private CerSampleTestTbMapper cerSampleTestTbMapper;
+    private BioSampleTestTbMapper bioSampleTestTbMapper;
 
     @Resource
     private CerPlantDtlTbMapper cerPlantDtlTbMapper;
@@ -75,8 +75,8 @@ public class ConversionAndTransProcServiceBase extends AbstractProjectBaseTaskSe
                 if (cerVectorTaskTb == null) {
                     throw new BusinessException("实施方案编号不存在：" + sample.getVectorTaskCode());
                 }
-                List<CerSampleTestTb> cerSampleTestTbList = cerSampleTestTbMapper.selectAllBySampleCode(sample.getSampleCode());
-                if (CollectionUtil.isEmpty(cerSampleTestTbList)) {
+                List<BioSampleTestTb> bioSampleTestTbList = bioSampleTestTbMapper.selectAllBySampleCode(sample.getSampleCode());
+                if (CollectionUtil.isEmpty(bioSampleTestTbList)) {
                     throw new BusinessException("此实施方案中无此取样编号：" + sample.getSampleCode());
                 }
                 //取样移苗时不校验是否是留种，如果把舍弃的苗移到CER,CER相关人员进行剔苗操作

2f0133d3aa016175512e946136b17537399ae1e2
zoujun <13683366156@163.com>
2025-12-05 10:21:40 +0800

    供应链管理项目初始化

--- bio-drqi-applet/src/main/java/com/bio/drqi/applet/contant/ScanCodeConstant.java
+++ bio-drqi-applet/src/main/java/com/bio/drqi/applet/contant/ScanCodeConstant.java
@@ -1,6 +1,7 @@
 package com.bio.drqi.applet.contant;
 
 import com.bio.drqi.applet.service.codescan.template.*;
+import com.bio.drqi.common.enums.PrintTypeEnum;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,18 +11,70 @@ public class ScanCodeConstant {
     public static final Map<String, Class> scanCodeClassMap=new HashMap<>();
 
     static {
-        scanCodeClassMap.put("transform_label_print", TransformCodeScanService.class);
-        scanCodeClassMap.put("transform_trans_print",transformTransCodeScanService.class);
+        //转化标签
+        scanCodeClassMap.put(PrintTypeEnum.transform_label_print.name() , TransformCodeScanService.class);
 
-        scanCodeClassMap.put("vector_label_print", PlasmidCodeScanService.class);
-        scanCodeClassMap.put("sample_label_small_project_print", ProjectSampleTestCodeScanService.class);
-        scanCodeClassMap.put("sample_label_large_project_print", ProjectSampleTestCodeScanService.class);
-        scanCodeClassMap.put("seed_in_label_print", SeedCodeScanService.class);
-        scanCodeClassMap.put("seed_out_label_print", SeedCodeScanService.class);
-        scanCodeClassMap.put("layout_number_label_print", NineSixLayoutCodeScanService.class);
-        scanCodeClassMap.put("plant_label_project_print", ProjectPlantCodeScanService.class);
-        scanCodeClassMap.put("tissue_embryo_label_print",TissueEmbryoCodeScanService.class);
-        scanCodeClassMap.put("bms_label_print",BmsCodeScanService.class);
+        //转化移苗标签
+        scanCodeClassMap.put(PrintTypeEnum.transform_trans_print.name() ,transformTransCodeScanService.class);
+
+        //载体构建标签
+        scanCodeClassMap.put(PrintTypeEnum.vector_label_print.name(), PlasmidCodeScanService.class);
+
+        //取样标签-小（项目）
+        scanCodeClassMap.put(PrintTypeEnum.sample_label_small_project_print.name() , ProjectSampleTestCodeScanService.class);
+
+        //取样标签-小（大）
+        scanCodeClassMap.put(PrintTypeEnum.sample_label_large_project_print.name() , ProjectSampleTestCodeScanService.class);
+
+        //种子入库标签
+        scanCodeClassMap.put(PrintTypeEnum.seed_in_label_print.name() , SeedCodeScanService.class);
+
+        //种子出库标签
+        scanCodeClassMap.put(PrintTypeEnum.seed_out_label_print.name() , SeedCodeScanService.class);
+
+        //96版标
+        scanCodeClassMap.put(PrintTypeEnum.layout_number_label_print.name(), NineSixLayoutCodeScanService.class);
+
+        //种植标签（项目）
+        scanCodeClassMap.put(PrintTypeEnum.plant_label_project_print.name(), ProjectPlantCodeScanService.class);
+
+        //组胚标签
+        scanCodeClassMap.put(PrintTypeEnum.tissue_embryo_label_print.name() ,TissueEmbryoCodeScanService.class);
+
+        //耗材标签
+        scanCodeClassMap.put(PrintTypeEnum.bms_label_print.name(),BmsCodeScanService.class);
+
+        //种植标签(CER)
+        scanCodeClassMap.put(PrintTypeEnum.plant_label_cer_print.name(),null);
+
+        //取样标签小（CER）
+        scanCodeClassMap.put(PrintTypeEnum.sample_label_small_cer_print.name() ,null);
+
+        //取样标签大（CER）
+        scanCodeClassMap.put(PrintTypeEnum.sample_label_large_cer_print.name(),null);
+
+        //种植申请标签(CER)
+        scanCodeClassMap.put(PrintTypeEnum.plant_apply_label_print.name(),null);
+
+        //试验申请标签(田测)
+        scanCodeClassMap.put(PrintTypeEnum.tc_experiment_label_print.name(),null);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 

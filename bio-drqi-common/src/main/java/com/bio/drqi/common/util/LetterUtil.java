c29d6d2dc48c0608284c913b3f05561340ae9fc3
zoujun <13683366156@163.com>
2025-12-31 09:29:54 +0800

    维护工具

--- bio-drqi-common/src/main/java/com/bio/drqi/common/util/LetterUtil.java
+++ bio-drqi-common/src/main/java/com/bio/drqi/common/util/LetterUtil.java
@@ -46,8 +46,13 @@ public class LetterUtil {
 
     }
 
+    public static boolean  isNumeric(String str) {
+        return str.matches("\\d+");
+    }
+
     public static void main(String[] args) {
-        System.out.println(nextLetterForInstantVerify("AZ"));
+        //System.out.println(nextLetterForInstantVerify("AZ"));
+        System.out.println(isNumeric("434"));
     }
 
 }

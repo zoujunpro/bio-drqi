efa47d19c115d9c1ebbe20b7ebf909ae6da96860
zoujun <13683366156@163.com>
2026-02-09 11:23:02 +0800

    优化金蝶数据同步，非生产环境不执行数据同步

--- bio-drqi-manage/bio-drqi-manage-service/src/main/java/com/bio/drqi/manage/dto/seed/DownSpotCheckResultExcelDTO.java
+++ bio-drqi-manage/bio-drqi-manage-service/src/main/java/com/bio/drqi/manage/dto/seed/DownSpotCheckResultExcelDTO.java
@@ -14,15 +14,6 @@ public class DownSpotCheckResultExcelDTO {
     @NotBlank(message = "excel中种子编号数据丢失")
     private String seedNum;
 
-    @ExcelProperty("取样编号")
-    private String sampleCode;
-
-    @ExcelProperty("取样数量")
-    private String sampleNumber;
-
-    @ExcelProperty("鉴定结果")
-    private String identityResult;
-
     @ExcelProperty("抽检反馈")
     private String spotCheckResult;
 }

36eb61aa33d7603a49f290a5fd5edc5ecde8246b
zoujun <zoujun@qi-biodesign.com>
2026-05-07 14:45:37 +0800

    工单打印

--- bio-drqi-es/src/main/java/com/bio/drqi/es/BioDrqiEsMain.java
+++ bio-drqi-es/src/main/java/com/bio/drqi/es/BioDrqiEsMain.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication(scanBasePackages = {"com.bio"})
 @EnableDiscoveryClient
 @MapperScan(basePackages = "com.bio.drqi.mapper")
-public class BioDrqiEsMain {
+public class
+BioDrqiEsMain {
 
     public static void main(String[] args) {
         SpringApplication.run(BioDrqiEsMain.class, args);

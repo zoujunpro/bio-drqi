867e1914ee9028dfa11a5ec915aee2622eabe062
zoujun <zoujun@qi-biodesign.com>
2026-06-11 11:15:49 +0800

    质粒农杆菌打印优化

--- bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/seed/SeedDestructionPageReqDTO.java
+++ bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/seed/SeedDestructionPageReqDTO.java
@@ -5,49 +5,198 @@ import lombok.Data;
 
 @Data
 public class SeedDestructionPageReqDTO extends PageDTO {
-    private String seedNum;
-    private String taskNum;
-    private String applyTaskNum;
-    private Integer applyUserId;
-    private String applyUserName;
-    private String destructionLocation;
-    private String destructionMethod;
-    private String sourceType;
-    private String plantCode;
-    private String parentNum;
-    private String fatherInfo;
-    private String matherInfo;
-    private String generation;
-    private String speciesCode;
-    private String breedCode;
-    private String pollinationMethod;
-    private String harvestType;
-    private String harvestTime;
-    private String productionLocationCode;
-    private String stockLocationNum;
-    private String targetCharacter;
-    private String aliasName;
-    private String geneType;
-    private String materialType;
-    private String matherSeedNum;
-    private String fatherSeedNum;
-    private String matherRegionNum;
-    private String fatherRegionNum;
-    private String genealogy;
-    private String geneSeparateFlag;
-    private String transFlag;
-    private String vectorTaskCode;
-    private String experimentNum;
-    private String projectCode;
-    private String fatherSingleNum;
-    private String matherSingleNum;
-    private String pdImplementCode;
     /**
-     * yyyyMMdd 销毁开始时间
+     * 种子编号
+     */
+    private String seedNum;
+
+    /**
+     * 销毁申请任务编号
+     */
+    private String taskNum;
+
+    /**
+     * 销毁申请任务编号，兼容前端 applyTaskNum 命名
+     */
+    private String applyTaskNum;
+
+    /**
+     * 申请人ID
+     */
+    private Integer applyUserId;
+
+    /**
+     * 申请人姓名
+     */
+    private String applyUserName;
+
+    /**
+     * 销毁地点
+     */
+    private String destructionLocation;
+
+    /**
+     * 销毁方法
+     */
+    private String destructionMethod;
+
+    /**
+     * 种子来源
+     */
+    private String sourceType;
+
+    /**
+     * 种植编号
+     */
+    private String plantCode;
+
+    /**
+     * 上一代种子编号
+     */
+    private String parentNum;
+
+    /**
+     * 父本信息
+     */
+    private String fatherInfo;
+
+    /**
+     * 母本信息
+     */
+    private String matherInfo;
+
+    /**
+     * 代次
+     */
+    private String generation;
+
+    /**
+     * 物种编码
+     */
+    private String speciesCode;
+
+    /**
+     * 品种编码
+     */
+    private String breedCode;
+
+    /**
+     * 授粉方式
+     */
+    private String pollinationMethod;
+
+    /**
+     * 收获方式
+     */
+    private String harvestType;
+
+    /**
+     * 收获时间
+     */
+    private String harvestTime;
+
+    /**
+     * 生产地点编码
+     */
+    private String productionLocationCode;
+
+    /**
+     * 库位编号
+     */
+    private String stockLocationNum;
+
+    /**
+     * 目标性状
+     */
+    private String targetCharacter;
+
+    /**
+     * 别名
+     */
+    private String aliasName;
+
+    /**
+     * 基因型
+     */
+    private String geneType;
+
+    /**
+     * 材料类型
+     */
+    private String materialType;
+
+    /**
+     * 母本种子编号
+     */
+    private String matherSeedNum;
+
+    /**
+     * 父本种子编号
+     */
+    private String fatherSeedNum;
+
+    /**
+     * 母本小区编号
+     */
+    private String matherRegionNum;
+
+    /**
+     * 父本小区编号
+     */
+    private String fatherRegionNum;
+
+    /**
+     * 系谱
+     */
+    private String genealogy;
+
+    /**
+     * 是否基因分离
+     */
+    private String geneSeparateFlag;
+
+    /**
+     * 是否转基因
+     */
+    private String transFlag;
+
+    /**
+     * 实施方案编号
+     */
+    private String vectorTaskCode;
+
+    /**
+     * 试验方案编号
+     */
+    private String experimentNum;
+
+    /**
+     * 项目编号
+     */
+    private String projectCode;
+
+    /**
+     * 父本单株编号
+     */
+    private String fatherSingleNum;
+
+    /**
+     * 母本单株编号
+     */
+    private String matherSingleNum;
+
+    /**
+     * PD号
+     */
+    private String pdImplementCode;
+
+    /**
+     * 销毁开始时间，格式：yyyyMMdd
      */
     private String beginDate;
+
     /**
-     * yyyyMMdd 销毁结束时间
+     * 销毁结束时间，格式：yyyyMMdd
      */
     private String endDate;
 }
--- bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/seed/SeedStockOutReqDTO.java
+++ bio-drqi-manage/bio-drqi-manage-dto/src/main/java/com/bio/drqi/manage/seed/SeedStockOutReqDTO.java
@@ -5,51 +5,198 @@ import lombok.Data;
 
 @Data
 public class SeedStockOutReqDTO extends PageDTO {
+    /**
+     * 种子编号
+     */
     private String seedNum;
 
-    /**seed_testing_apply 考种  seed_breed_apply繁种  seed_preparation_apply备种  seed_other_out_apply其他 seed_destruction_apply种子销毁  */
-    private String useToCode;
-    private String taskNum;
-    private String outTaskNum;
-    private String useToDesc;
-    private String sourceType;
-    private Integer applyUserId;
-    private String applyUserName;
-    private String plantCode;
-    private String parentNum;
-    private String fatherInfo;
-    private String matherInfo;
-    private String generation;
-    private String speciesCode;
-    private String breedCode;
-    private String pollinationMethod;
-    private String harvestType;
-    private String harvestTime;
-    private String productionLocationCode;
-    private String stockLocationNum;
-    private String targetCharacter;
-    private String aliasName;
-    private String geneType;
-    private String materialType;
-    private String matherSeedNum;
-    private String fatherSeedNum;
-    private String matherRegionNum;
-    private String fatherRegionNum;
-    private String genealogy;
-    private String geneSeparateFlag;
-    private String transFlag;
-    private String vectorTaskCode;
-    private String experimentNum;
-    private String projectCode;
-    private String fatherSingleNum;
-    private String matherSingleNum;
-    private String pdImplementCode;
     /**
-     * yyyyMMdd 出库开始时间
+     * 用途编码：seed_testing_apply 考种，seed_breed_apply 繁种，seed_preparation_apply 备种，seed_other_out_apply 其他，seed_destruction_apply 种子销毁
+     */
+    private String useToCode;
+
+    /**
+     * 出库申请任务编号
+     */
+    private String taskNum;
+
+    /**
+     * 出库流水编号
+     */
+    private String outTaskNum;
+
+    /**
+     * 用途描述
+     */
+    private String useToDesc;
+
+    /**
+     * 种子来源
+     */
+    private String sourceType;
+
+    /**
+     * 申请人ID
+     */
+    private Integer applyUserId;
+
+    /**
+     * 申请人姓名
+     */
+    private String applyUserName;
+
+    /**
+     * 种植编号
+     */
+    private String plantCode;
+
+    /**
+     * 上一代种子编号
+     */
+    private String parentNum;
+
+    /**
+     * 父本信息
+     */
+    private String fatherInfo;
+
+    /**
+     * 母本信息
+     */
+    private String matherInfo;
+
+    /**
+     * 代次
+     */
+    private String generation;
+
+    /**
+     * 物种编码
+     */
+    private String speciesCode;
+
+    /**
+     * 品种编码
+     */
+    private String breedCode;
+
+    /**
+     * 授粉方式
+     */
+    private String pollinationMethod;
+
+    /**
+     * 收获方式
+     */
+    private String harvestType;
+
+    /**
+     * 收获时间
+     */
+    private String harvestTime;
+
+    /**
+     * 生产地点编码
+     */
+    private String productionLocationCode;
+
+    /**
+     * 库位编号
+     */
+    private String stockLocationNum;
+
+    /**
+     * 目标性状
+     */
+    private String targetCharacter;
+
+    /**
+     * 别名
+     */
+    private String aliasName;
+
+    /**
+     * 基因型
+     */
+    private String geneType;
+
+    /**
+     * 材料类型
+     */
+    private String materialType;
+
+    /**
+     * 母本种子编号
+     */
+    private String matherSeedNum;
+
+    /**
+     * 父本种子编号
+     */
+    private String fatherSeedNum;
+
+    /**
+     * 母本小区编号
+     */
+    private String matherRegionNum;
+
+    /**
+     * 父本小区编号
+     */
+    private String fatherRegionNum;
+
+    /**
+     * 系谱
+     */
+    private String genealogy;
+
+    /**
+     * 是否基因分离
+     */
+    private String geneSeparateFlag;
+
+    /**
+     * 是否转基因
+     */
+    private String transFlag;
+
+    /**
+     * 实施方案编号
+     */
+    private String vectorTaskCode;
+
+    /**
+     * 试验方案编号
+     */
+    private String experimentNum;
+
+    /**
+     * 项目编号
+     */
+    private String projectCode;
+
+    /**
+     * 父本单株编号
+     */
+    private String fatherSingleNum;
+
+    /**
+     * 母本单株编号
+     */
+    private String matherSingleNum;
+
+    /**
+     * PD号
+     */
+    private String pdImplementCode;
+
+    /**
+     * 出库开始时间，格式：yyyyMMdd
      */
     private String beginDate;
+
     /**
-     * yyyyMMdd 出库结束时间
+     * 出库结束时间，格式：yyyyMMdd
      */
     private String endDate;
 
